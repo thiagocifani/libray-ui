59e0bdce4da86c7a08101b161b267340f59f940d
thiagocifani <cifani.thiago@gmail.com>
2021-01-16 20:34:01 -0300

    Add router and detail templates

---
+++ app/controllers/author/detail.js
@@ -0,0 +1,11 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+
+export default class AuthorDetailController extends Controller {
+
+  @action
+  async deleteAuthor(author) {
+    await author.destroyRecord();
+    this.transitionToRoute('author.index');
+  }
+}
