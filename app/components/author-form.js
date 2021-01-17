ccbcb3503581521118506f3615f4e4ef22a53b1c
thiagocifani <cifani.thiago@gmail.com>
2021-01-16 21:40:50 -0300

    Add author form component.

---
+++ app/components/author-form.js
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component';
+import { action} from '@ember/object'
+import { tracked } from '@glimmer/tracking';
+
+export default class AuthorFormComponent extends Component {
+  @tracked first = this.args.author.first;
+  @tracked last = this.args.author.last;
+
+  @action
+  submitChanges(ev) {
+    ev.preventDefault();
+
+    console.log("teste");
+
+    this.args.onsubmit({
+      first: this.first,
+      last: this.last
+    })
+  }
+}
