efb99096de839aed6bcc7991c9cc7f7afaf1cad2
thiagocifani <cifani.thiago@gmail.com>
2021-01-17 17:34:39 -0300

    Add filter author with query params

--- app/routes/author.js
+++ app/routes/author.js
@@ -1,7 +1,22 @@
 import Route from '@ember/routing/route';
 
 export default class AuthorRoute extends Route {
-  model() {
+  queryParams =  {
+    search: {
+      refreshModel: true
+    }
+  }
+
+  model({ search }) {
+    if(search) {
+      // /authors?filter[query]=J
+      return this.store.query('author', {
+        filter: {
+          query: search
+        }
+      });
+    }
+
     return this.store.findAll('author');
   }
 }
