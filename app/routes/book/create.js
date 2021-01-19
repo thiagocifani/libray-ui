f7aef4c50ecb1081ea8922b8be5a9ac1d948648d
thiagocifani <cifani.thiago@gmail.com>
2021-01-18 23:55:44 -0300

    Add book create route

--- app/routes/book/create.js
+++ app/routes/book/create.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class BookCreateRoute extends Route {
+  model() {
+    return {
+      title: '',
+      isbn: '',
+      publishDate: null,
+      author: null
+    };
+  }
 }
